b3d44ac31759203afb7cb7421c1ce5539009881a
Bonfell <porkhunov99@inbox.ru>
2026-06-04 22:13:04 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -28,6 +28,7 @@ class PMTree {
   static void buildSubtree(Node* node, std::vector<char> remaining);
 
   static void deleteSubtree(Node* node);
+  static void collectPerms(const Node* node, std::vector<char>& current, std::vector<std::vector<char>>& out);
 };
 
 #endif  // INCLUDE_TREE_H_
